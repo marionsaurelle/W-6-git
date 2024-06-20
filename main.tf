c002c543839cb80b2fd531cf514f7501c9cefd7c
saurelle <msmkamdem@gmail.com>
2024-06-20 08:41:06 -0500

    lightsail

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_lightsail_instance" "server1" {
   name = "dev-server"
   blueprint_id = "ubuntu_20_04"
   bundle_id = "medium_1_0"
-  availability_zone = "us-east-1a"
+  availability_zone = "us-east-1"
    user_data = <<-EOF
               #!/bin/bash
               sudo apt-get update
